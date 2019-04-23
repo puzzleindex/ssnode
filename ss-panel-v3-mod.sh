2a668a2cc05e831e9fe44557d081794d65793ff4
Rottyler <38752655+Rottyler@users.noreply.github.com>
2019-04-23 09:04:21 +0800

    Update ss-panel-v3-mod.sh

--- ss-panel-v3-mod.sh
+++ ss-panel-v3-mod.sh
@@ -139,7 +139,7 @@ install_centos_ssr(){
 	./configure && make -j2 && make install
 	echo /usr/local/lib > /etc/ld.so.conf.d/usr_local_lib.conf
 	ldconfig
-	git clone -b manyuser https://github.com/Rottyler/shadowsocks.git "/root/shadowsocks"
+	git clone -b manyuser https://github.com/Rottyler/uimnode.git "/root/shadowsocks"
 	cd /root/shadowsocks
 	chkconfig supervisord on
 	#第一次安装
@@ -186,7 +186,7 @@ install_ubuntu_ssr(){
 	apt-get install python-pip git -y
 	pip install cymysql
 	cd /root
-	git clone -b manyuser https://github.com/Rottyler/shadowsocks.git "/root/shadowsocks"
+	git clone -b manyuser https://github.com/Rottyler/uimnode.git "/root/shadowsocks"
 	cd shadowsocks
 	pip install -r requirements.txt
 	chmod +x *.sh
